8a3402d8a25a2bdf91c7c2b14f0193b98cf7f740
liaogd <285282227@qq.com>
2021-01-05 14:58:09 +0800

    1.0.0

---
+++ XHCloudTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XHCloudTalk
+//
+//  Created by DONG on 2021/1/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
