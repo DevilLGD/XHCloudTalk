7c1835c160ab7bf518e15bc27432d522d5a89273
liaogd <285282227@qq.com>
2021-01-06 15:13:53 +0800

    1.0.1

--- XHCloudTalk/ViewController.swift
+++ XHCloudTalk/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
         
         //XHCloudTalkSwiftHeader().show(str: "")
         
-        //成功
+        //成功2
     }
 
 
---
+++ XHCloudTalk/XHTest.swift
@@ -0,0 +1,12 @@
+//
+//  XHTest.swift
+//  XHCloudTalk
+//
+//  Created by DONG on 2021/1/6.
+//
+
+import UIKit
+
+class XHTest: NSObject {
+
+}
